3f9c89189e40cac492581b6bf44290937cab167d
AHMADKHATIB <Ahmadkhatib0@gmail.com>
2021-10-04 11:06:56 +0300

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,77 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full flex">
+    <div class="m-auto">
+      <h1 class="text-center text-2xl">Dc heros {{ herosCount }}</h1>
+      <ul>
+        <li
+          class="justify-between flex"
+          v-for="(hero, index) in dcHeros"
+          v-bind:key="hero.name"
+        >
+          <div>
+            {{ hero.name }}
+          </div>
+          <button v-on:click="remove(index)">x</button>
+        </li>
+      </ul>
+
+      <form @submit.prevent="addHero" class="m-10">
+        <input
+          v-model="newHero"
+          class="border rounded"
+          placeholder="Type hero name here "
+        />
+        <button
+          type="submit"
+          class="
+            border
+            rounded
+            bg-gradient-to-r
+            from-red-700
+            to to-pink-500
+            text-white
+          "
+        >
+          add hero
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  computed: {
+    herosCount() {
+      return this.dcHeros.length;
+    },
+  },
+  methods: {
+    addHero() {
+      if (this.newHero !== "") {
+        this.dcHeros.push({ name: this.newHero });
+        this.newHero = "";
+      }
+    },
+
+    remove(index) {
+      this.dcHeros = this.dcHeros.filter((hero, i) => i !== index);
+    },
+  },
+
+  data() {
+    return {
+      newHero: "aquaman",
+      //  dcHeros :  ['superGirl' ,"batMan" , 'Superman' ]
+      dcHeros: [
+        { name: "superGirl" },
+        { name: "batMan" },
+        { name: "Superman" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
